b561af470c0ebc53dac2e882116ff060c35bb177
Calvin Zhu <apexchu@hotmail.com>
2022-06-03 18:08:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,35 +33,35 @@ CONFPATH=/config/scripts
 
 #copy pdnsd and trojan to /usr/bin
 test -d $PATH || exit 0
-cp -f y bin/mips64/* $PATH
+sudo cp -f y bin/mips64/* $PATH
 [ -z "$RUNAS" ] && RUNAS=nobody
 #chown +x for pdnsd and trojan
-chown $RUNAS $PATH/pdnsd
-chown $RUNAS $PATH/pdnsd-ctl
-chown $RUNAS $PATH/trojan
+sudo chown $RUNAS $PATH/pdnsd
+sudo chown $RUNAS $PATH/pdnsd-ctl
+sudo chown $RUNAS $PATH/trojan
 #chown +x for pdnsd and trojan
-chmod +x $PATH/pdnsd
-chmod +x $PATH/pdnsd-ctl
-chmod +x $PATH/trojan
+sudo chmod +x $PATH/pdnsd
+sudo chmod +x $PATH/pdnsd-ctl
+sudo chmod +x $PATH/trojan
 
 #create /lib32 directory and copy library fils to /lib32
 test -d /lib32 || mkdir /lib32
-cp -f y lib32/* /lib32
-chown -r $RUNAS /lib32
+sudo cp -f y lib32/* /lib32
+sudo chown -r $RUNAS /lib32
 
 #trojan
 test -d $CONFPATH/trojan || mkdir $CONFPATH/trojan
-sed -i "s|{ip}|$SERVER_IP|g" conf.d/trojan-tcp-udp.json
-sed -i "s|{domainame}|$SERVER_DOMAINAME|g" conf.d/trojan-tcp-udp.json
-sed -i "s|{port}|$SERVER_PORT|g" conf.d/trojan-tcp-udp.json
-sed -i "s|{pass}|$SERVER_PASS|g" conf.d/trojan-tcp-udp.json
+sudo sed -i "s|{ip}|$SERVER_IP|g" conf.d/trojan-tcp-udp.json
+sudo sed -i "s|{domainame}|$SERVER_DOMAINAME|g" conf.d/trojan-tcp-udp.json
+sudo sed -i "s|{port}|$SERVER_PORT|g" conf.d/trojan-tcp-udp.json
+sudo sed -i "s|{pass}|$SERVER_PASS|g" conf.d/trojan-tcp-udp.json
 cp -f y conf.d/trojan-tcp-udp.json $CONFPATH/trojan
 
 
 #pdnsd
 PDNSCACHE=/var/cache/pdnsd
 test -d  $PDNSCACHE || mkdir $PDNSCACHE
-sed -i "s|{cache}|$PDNSCACHE|g" conf.d/pdnsd.conf
+sudo sed -i "s|{cache}|$PDNSCACHE|g" conf.d/pdnsd.conf
 cp -f y conf.d/pdnsd.conf $CONFPATH/trojan
 
 #dnsmasq
@@ -81,23 +81,23 @@ CONF_WITH_SERVERS=(accelerated-domains.china)
 CONF_SIMPLE=(bogus-nxdomain.china)
 echo "Removing old configurations..."
 for _conf in "${CONF_WITH_SERVERS[@]}" "${CONF_SIMPLE[@]}"; do
-rm -f /etc/dnsmasq.d/"$_conf"*.conf
+sudo rm -f /etc/dnsmasq.d/"$_conf"*.conf
 done
 
 echo "Installing new configurations..."
 for _conf in "${CONF_SIMPLE[@]}"; do
-cp "$WORKDIR/$_conf.conf" "/etc/dnsmasq.d/$_conf.conf"
+sudo cp "$WORKDIR/$_conf.conf" "/etc/dnsmasq.d/$_conf.conf"
 done
 
 for _server in "${SERVERS[@]}"; do
 for _conf in "${CONF_WITH_SERVERS[@]}"; do
-cp "$WORKDIR/$_conf.conf" "/etc/dnsmasq.d/$_conf.$_server.conf"
+sudo cp "$WORKDIR/$_conf.conf" "/etc/dnsmasq.d/$_conf.$_server.conf"
 done
 
-sed -i "s|^\(server.*\)/[^/]*$|\1/$_server|" /etc/dnsmasq.d/*."$_server".conf
+sudo sed -i "s|^\(server.*\)/[^/]*$|\1/$_server|" /etc/dnsmasq.d/*."$_server".conf
 done
 
-touch /etc/dnsmasq.d/final.conf
+sudo touch /etc/dnsmasq.d/final.conf
 echo 'server=/#/127.0.0.1#5335' > /etc/dnsmasq.d/final.conf
 echo 'conf-dir=/etc/dnsmasq.d/,*.conf' >> /etc/dnsmasq.conf
 
@@ -105,7 +105,7 @@ echo "Restarting dnsmasq service..."
 sudo service dnsmasq restart
 
 echo "Cleaning up..."
-rm -r "$WORKDIR"
+sudo rm -r "$WORKDIR"
 
 #tptables
 #curl 'https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt' > cn_ipv4.list
@@ -115,9 +115,9 @@ wait
 CHAIN_NAME='BYPASSLIST'
 
 #del chnroute
-ipset destroy chnroute
-iptables -t nat -F $CHAIN_NAME
-iptables -t nat -X $CHAIN_NAME
+sudo ipset destroy chnroute
+sudo iptables -t nat -F $CHAIN_NAME
+sudo iptables -t nat -X $CHAIN_NAME
 echo 'Del_rules Done.'
 
 # Add new ipset
@@ -125,46 +125,46 @@ ipset destroy chnlist
 ipset -N chnlist hash:net maxelem 65536
 
 echo 'ipset processing...'
-ipset add chnlist $SERVER_IP
-ipset add chnlist 0.0.0.0/8
-ipset add chnlist 10.0.0.0/8
-ipset add chnlist 127.0.0.0/8
-ipset add chnlist 169.254.0.0/16
-ipset add chnlist 172.16.0.0/12
-ipset add chnlist 192.168.0.0/16
-ipset add chnlist 224.0.0.0/4
-ipset add chnlist 240.0.0.0/4
+sudo ipset add chnlist $SERVER_IP
+sudo ipset add chnlist 0.0.0.0/8
+sudo ipset add chnlist 10.0.0.0/8
+sudo ipset add chnlist 127.0.0.0/8
+sudo ipset add chnlist 169.254.0.0/16
+sudo ipset add chnlist 172.16.0.0/12
+sudo ipset add chnlist 192.168.0.0/16
+sudo ipset add chnlist 224.0.0.0/4
+sudo ipset add chnlist 240.0.0.0/4
 
-for ip in $(cat cn_ipv4.list)
+for ip in $(sudo cat cn_ipv4.list)
 do
-    ipset add chnlist $ip
+    sudo ipset add chnlist $ip
 done
 echo 'ipset done.'
 
 # 1. TCP
 # TCP new chain $CHAIN_NAME
-iptables -t nat -N $CHAIN_NAME
+sudo iptables -t nat -N $CHAIN_NAME
 # TCP ipset match
-iptables -t nat -A $CHAIN_NAME -p tcp -m set --match-set chnlist dst -j RETURN
+sudo iptables -t nat -A $CHAIN_NAME -p tcp -m set --match-set chnlist dst -j RETURN
 # TCP redirect
-iptables -t nat -A $CHAIN_NAME -p tcp -j REDIRECT --to-ports 1234
+sudo iptables -t nat -A $CHAIN_NAME -p tcp -j REDIRECT --to-ports 1234
 # TCP rule to prerouting chain
-iptables -t nat -A PREROUTING -p tcp -j $CHAIN_NAME
+sudo iptables -t nat -A PREROUTING -p tcp -j $CHAIN_NAME
 # For local
 #iptables -t nat -I OUTPUT -p tcp -j $CHAIN_NAME
 echo 'iptables setting is done.'
 
 #supervisord
-sed -i "s|{path}|$PATH|g" conf.d/trojan-supervisord.conf
-sed -i "s|{configpath}|$CONFPATH|g" conf.d/trojan-supervisord.conf
-cp conf.d/trojan-supervisord.conf /etc/supervisor/conf.d/shadowsocks.conf
+sudo sed -i "s|{path}|$PATH|g" conf.d/trojan-supervisord.conf
+sudo sed -i "s|{configpath}|$CONFPATH|g" conf.d/trojan-supervisord.conf
+sudo cp conf.d/trojan-supervisord.conf /etc/supervisor/conf.d/shadowsocks.conf
 sudo supervisorctl shutdown ; sudo supervisord
 
 #auto update chnipsets
-sed -i "s|# SERVER_IP|SERVER_IP=$SERVER_IP|g" iptables.sh
-cp -f y iptables.sh $CONFPATH/trojan
-chown $RUNAS $CONFPATH/trojan/iptables.sh
-chmod +x $CONFPATH/trojan/iptables.sh
-sed '$a* 3 * * * $CONFPATH/trojan/iptables.sh add_rules' /etc/crontab
+sudo sed -i "s|# SERVER_IP|SERVER_IP=$SERVER_IP|g" iptables.sh
+sudo cp -f y iptables.sh $CONFPATH/trojan
+sudo chown $RUNAS $CONFPATH/trojan/iptables.sh
+sudo chmod +x $CONFPATH/trojan/iptables.sh
+sudo sed '$a* 3 * * * $CONFPATH/trojan/iptables.sh add_rules' /etc/crontab
 
 echo "All Done"
